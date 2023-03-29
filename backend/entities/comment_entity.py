cd3777d1a875bd620e3d25111ee7e7b8a608aee2
Chalisa Keaw Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-03-29 14:04:06 -0400

    Data models wip (#19)
    * Add post and comment model
    
    * Add team model
    
    * Update user.py to include posts and teams
    
    * update entities
    
    none have been tests, updates based on my code and kris jordan's
    
    added comment, post, and team according to models created previously
    
    reply and membership are association tables
    
    * Update user_entity.py
    
    updated to match previous updates to model.
    
    methods were also updated.
    
    nothiing tested so far.
    
    TODO: update creation and reset of databse.
    
    * Fix typo in our User model
    
    Error: TypeError: 'type' object does not support item assignment
    
    Solution: removed the first = sign because it was supposed to be a colon (:). No idea why I typed it worng in the first place.
    
    * Update post.py
    
    added this line because I thought it might be necessary to have a default value here.
    
    * Update post.py
    
    Error: " TypeError: unsupported operand type(s) for |: 'str' and 'type'"
    
    Solution: Since I've faced issues in the past with having the model encapsulate another data type (it can cause an infiinite loop since the User contains a list of posts)  I have decided that the Post will hold User information by holding the poster's PID or just primary key ID number.
    
    * Update comment.py
    
    Error: TypeError: unsupported operand type(s) for |: 'str' and 'type'
    
    Solution: see previous commit about the updates to post.
    
    Comment will hold the primary key or PID of the User and primary key of the post.
    
    * Update team.py
    
    Error: TypeError: 'type' object does not support item assignment
    
    Solution: see previous commit about the updates to User model
    
    * Update team_entity.py
    
    potential fix, devcontainer not working
    
    but basically it should backpopulate the corract propety in User entity

---
+++ backend/entities/comment_entity.py
@@ -0,0 +1,62 @@
+'''Comments for all registered users in the application.'''
+
+
+from sqlalchemy import Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+# from sqlalchemy.ext.mutable import MutableList
+from typing import Self
+from datetime import datetime
+from .entity_base import EntityBase
+from .reply_entity import reply_table
+from ..models import Comment
+
+
+# __authors__ = ['Chalisa Phoomsakha']
+# __copyright__ = 'Copyright 2023'
+# __license__ = 'MIT'
+
+
+class CommentEntity(EntityBase):
+    __tablename__ = 'comment'
+
+    id = mapped_column(Integer, primary_key=True)
+
+    user_id = mapped_column(ForeignKey("user.PID"))
+    commenter: Mapped['UserEntity'] = relationship(post_update=True)
+
+    post_id = mapped_column(ForeignKey("post.id"))
+    post: Mapped['PostEntity'] = relationship(back_populates="comments", post_update=True)
+
+    # not sure ifi this line is necessary, i am combining kris jordan's and my own code
+    # replyTo_id = mapped_column(ForeignKey("comments.id"))
+    replies: Mapped[list["CommentEntity"]] = relationship(secondary="reply", primaryjoin=id==reply_table.c.comment_id,
+                            secondaryjoin=id==reply_table.c.reply_id, back_populates="replies", post_update=True)
+    
+    text: Mapped[str] = mapped_column(String(64))
+    created: Mapped[datetime] = mapped_column(DateTime)
+
+    @classmethod
+    def from_model(cls, model: Comment) -> Self:
+        return cls(
+            id=model.id,
+            text=model.text,
+            created=model.created,
+            commenter=model.commenter,
+            post=model.post,
+            replies=model.replies
+        )
+
+    def to_model(self) -> Comment:
+        return Comment(
+            id=self.id,
+            text=self.text,
+            commenter=self.commenter,
+            created=self.created,
+            post=self.post,
+            replies=self.replies,
+        )
+
+    # not sure if necessary
+    # def update(self, model: Post) -> None:
+    #     self.content = model.content
+    #     self.tags = model.tagss
---
+++ backend/models/post.py
@@ -0,0 +1,27 @@
+"""Post model serves as the data object for representing posts across application layers."""
+
+from pydantic import BaseModel
+from datetime import datetime
+
+# not sure if i need this lol
+# __authors__ = ["Chalisa Phoomsakha"]
+# __copyright__ = "Copyright 2023"
+# __license__ = "MIT"
+
+#: Post object
+class Post(BaseModel):
+    # this is the primary key
+    id: int | None = None
+    content: str = ""
+    created: datetime = datetime.now()
+    postedBy: int | None
+    comments: list['Comment'] = []
+    tags: list[str] = []
+    # class Config:
+    #     orm_mode = True
+
+# copied fro professor's databse code at the end of User Model.. Assuming theres some importance here
+# Python... :sob:... necessary due to circularity (TODO: refactor to remove circularity)
+from .user import User
+from .comment import Comment
+Post.update_forward_refs()
