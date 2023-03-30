30e52f723b936ab3fb5b31178cb62250d084e232
Guning Shen <gunings@email.unc.edu>
2023-03-30 11:18:04 -0400

    Backend task2 wip (#23)
    * Add create post methods to API routes and services
    
    * Add some error handling

--- backend/api/post.py
+++ backend/api/post.py
@@ -1,1 +1,18 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from ..services.post import PostService
+from ..models.post import Post
+
+openapi_tags = {"name": "Post view", "description": "Post projects endpoints."}
+
+api = APIRouter(prefix="/api/post")
+
+@api.get("", tag=["Post view"])
+def get_posts(post_serv: PostService = Depends()) -> list[Post]:
+    return post_serv.get()
+
+@api.post("", tag=["Post view"])
+def create_post(post: Post, post_serv: PostService = Depends()) -> Post:
+    try:
+        return post_serv.create(post)
+    except Exception as e:
+        raise HTTPException(status_code=422, detail=str(e))
