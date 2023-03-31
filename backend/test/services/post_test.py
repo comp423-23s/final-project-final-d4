b5d68ef0bfc3db390b10ac9c77ec0a41aecfa77b
GuningShen <gunings@email.unc.edu>
2023-03-31 11:03:18 -0400

    Fix a small bug in create post

--- backend/test/services/post_test.py
+++ backend/test/services/post_test.py
@@ -6,7 +6,6 @@ from ...services import PostService
 
 # Model post 
 sample_post = Post(id=1, content="Welcome to csxl!", postedBy=1, comments=[])
-false_post = Post(id=2, postedBy=2)
 # Model comment
 sample_comment_1 = Comment(id=1, commenter=1, text="Hello", post=1, replies=[])
 # Model user
@@ -16,10 +15,10 @@ team_1 = Team(id=1, members=[user], project="1")
 
 @pytest.fixture(autouse=True)
 def test_create_post_valid(post: PostService):
+    assert(len(post.get_posts()) == 0)
+    
     post.create_post(sample_post)
     assert(len(post.get_posts()) == 1)
 
-# def test_create_post_invalid_content(post: PostService):
-#     with pytest.raises(Exception):
-#         post.create_post(false_post)
-# TODO: test on create posts with invalid content
+    post.create_post(sample_post)
+    assert(len(post.get_posts()) == 2)
