9530e8cd114544433d146b8d846b0aea0d68d310
Wenjing Huang <rubyhwj@email.unc.edu>
2023-03-31 14:28:30 -0400

    unify the backend and the frontend by unify the structure of post

--- backend/test/services/test_post.py
+++ backend/test/services/test_post.py
@@ -6,7 +6,7 @@ from ...services import PostService
 
 # Model post 
 sample_post = Post(id=1, content="Welcome to csxl!", postedBy=1, comments=[])
-false_post = Post(id=2, postedBy=2)
+false_post = Post(id=2, postedBy=2,content="")
 # Model comment
 sample_comment_1 = Comment(id=1, commenter=1, text="Hello", post=1, replies=[])
 # Model user
@@ -14,12 +14,13 @@ user = User(id=3, pid=111111111, onyen='user', email='user@unc.edu')
 # Model team
 team_1 = Team(id=1, members=[user], project="1")
 
-@pytest.fixture(autouse=True)
+
 def test_create_post_valid(post: PostService):
     post.create_post(sample_post)
-    assert(len(post.get_posts()) == 1)
+    assert(post.get_posts() != None)
 
-# def test_create_post_invalid_content(post: PostService):
-#     with pytest.raises(Exception):
-#         post.create_post(false_post)
-# TODO: test on create posts with invalid content
+
+def test_create_post_invalid_content(post: PostService):
+    with pytest.raises(Exception):         
+        post.create_post(false_post)
+
