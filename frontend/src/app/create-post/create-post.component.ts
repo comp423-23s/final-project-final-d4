6fb275cc22da6d6375d680b7c2c8bbf604ae42c5
GuningShen <gunings@email.unc.edu>
2023-04-10 17:03:12 -0400

    Fetch user profile for create post

--- frontend/src/app/create-post/create-post.component.ts
+++ frontend/src/app/create-post/create-post.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Post, PostsService } from '../post.service';
+import { Observable, Subscription } from 'rxjs';
+import { ProfileService, Profile } from '../profile/profile.service';
 
 @Component({
   selector: 'app-create-post',
@@ -8,21 +10,24 @@ import { Post, PostsService } from '../post.service';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent {
+  public profile$: Observable<Profile | undefined>;
 
   constructor(
-    public postService: PostsService
-  ) {}
+    public postService: PostsService,
+    private profileService: ProfileService
+  ) {
+    this.profile$ = profileService.profile$;
+  }
 
 
   onPost(form: NgForm):void{
+    
     let postedBy = parseInt(form.value.id ?? "");
     let title = (form.value.title ?? "");
     let content = (form.value.content ?? "");
     let description = (form.value.description ?? "");
     let time = (form.value.dateTime)
     let tag = (form.value.tag ?? "");
-    // this.postService.addPost(form.value.id, form.value.title, form.value.descripiton, form.value.content, form.value.dateTime, form.value.tag);
-    // 
 
     this.postService
       .addPost(postedBy, title, content, description, time, tag)
