7fc51f52f63c2329d687c99719d05619bb2aa695
Wenjing Huang <rubyhwj@email.unc.edu>
2023-04-11 15:49:57 -0400

    implement the autocomplete chip into the tags entry, still need some adjustment

--- frontend/src/app/create-post/create-post.component.ts
+++ frontend/src/app/create-post/create-post.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Post, PostsService } from '../post.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {FormControl} from '@angular/forms';
+import { Observable } from 'rxjs';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.css']
 })
+
 export class CreatePostComponent {
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  tagCtrl = new FormControl('');
+  filteredTags: Observable<string[]> | undefined;
+  tags: string[] = ['Lemon'];
+  allTags: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+  @ViewChild('tagInput')
+  tagInput!: ElementRef<HTMLInputElement>;
+
 
   constructor(
     public postService: PostsService
@@ -46,6 +61,42 @@ export class CreatePostComponent {
     } else {
       window.alert("Unknown error: " + JSON.stringify(err));
     }
+  }
+
+  // For tags part
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our tag
+    if (value) {
+      this.tags.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+
+    this.tagCtrl.setValue(null);
+  }
+
+  remove(fruit: string): void {
+    const index = this.tags.indexOf(fruit);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.tags.push(event.option.viewValue);
+    this.tagInput.nativeElement.value = '';
+    this.tagCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allTags.filter(tag => tag.toLowerCase().includes(filterValue));
   }
 
 
