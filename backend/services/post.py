e9d7407720b9b8b15ce30e964a4d48829db1a285
GuningShen <gunings@email.unc.edu>
2023-04-12 14:15:28 -0400

    Working on pytest session

--- backend/services/post.py
+++ backend/services/post.py
@@ -11,11 +11,11 @@ from sqlalchemy import create_engine
 
 
 class PostService:
-    # _session: Session
+    _session: Session
     _permission: PermissionService
 
     def __init__(self, session: Session = Depends(db_session), permission: PermissionService = Depends()):
-        # self._session = session
+        self._session = session
         self._permission = permission
 
     @staticmethod
@@ -24,17 +24,17 @@ class PostService:
         return Session(bind=engine)
 
     # Get all posts
-    def get_posts(self, session: Session = None) -> list[Post] | None:
-        if session is None:
-            session = self.create_session()
-        query = session.query(PostEntity)
+    def get_posts(self) -> list[Post] | None:
+        # if session is None:
+        #     session = self.create_session()
+        query = self._session.query(PostEntity)
         entities = query.all()
         return [entity.to_model() for entity in entities]
 
     # Search posts
-    def search_post(self, query: str, session: Session = None) -> list[Post] | None:
-        if session is None:
-            session = self.create_session()
+    def search_post(self, query: str) -> list[Post] | None:
+        # if session is None:
+        #     session = self.create_session()
         
         statement = select(PostEntity)
         criteria = or_(
@@ -47,28 +47,28 @@ class PostService:
         return [entity.to_model() for entity in entities]
     
     # Create new post
-    def create_post(self, post: Post, user: User, session: Session = None) -> Post | None:
-        if session is None:
-            session = self.create_session()
+    def create_post(self, post: Post, user: User) -> Post | None:
+        # if session is None:
+        #     session = self.create_session()
         
         userEntity = UserEntity.from_model(user)
         post_entity = PostEntity.from_model(post)
         post_entity.postedBy = userEntity
-        session.add(post_entity)
-        session.flush()
-        session.commit()
+        self._session.add(post_entity)
+        self._session.flush()
+        self._session.commit()
         return post_entity.to_model()
     
     # Delete post
-    def delete_post(self, id: int, session: Session=None) -> Post | None:
-        if session is None:
-            session = self.create_session()
+    def delete_post(self, id: int) -> Post | None:
+        # if session is None:
+        #     session = self.create_session()
 
         for i in self.get_posts():
             if i.id == id:
-                post_entity = session.query(PostEntity).filter(PostEntity.id == id).one()
-                session.delete(post_entity)
-                session.commit()
+                post_entity = self._session.query(PostEntity).filter(PostEntity.id == id).one()
+                self._session.delete(post_entity)
+                self._session.commit()
                 return post_entity
         
         raise ValueError("The post is not in the system.")
--- backend/test/services/post_test.py
+++ backend/test/services/post_test.py
@@ -9,10 +9,10 @@ from ...services import PostService,UserService
 from ...entities import UserEntity, PostEntity
 
 # Model post 
-sample_post = Post(id=5,content="Good Day", tags=["Daily"], title="Greeting", description="Good morrow students!")
+sample_post = Post(id=1,content="Good Day", tags=["Daily"], title="Greeting", description="Good morrow students!")
 
 # Model user
-user1 = User(id=5, pid=999999998, onyen="one", first_name="abc", last_name="abc", email="abc@unc.edu", pronouns="she/her/hers")
+user4 = User(id=5, pid=999999998, onyen="one", first_name="abc", last_name="abc", email="abc@unc.edu", pronouns="she/her/hers")
 
 # Model comment
 sample_comment_1 = Comment(id=1, commenter=1, text="Hello", post=1, replies=[])
@@ -30,15 +30,18 @@ def post(test_session: Session):
     permission_service = PermissionService(test_session)
     return PostService(session=test_session,permission=permission_service)
 
-def test_empty_post(post: PostService):
-    assert(len(post.get_posts()) == 4)
+@pytest.fixture()
+def users(test_session: Session):
+    return UserService(test_session)
 
-def test_create_post(post: PostService, test_session: Session):
-    user_1_entity = UserEntity.from_model(user1)
-    test_session.add(user_1_entity)
-    post.create_post(sample_post, user1)
-    assert(len(post.get_posts()) == 5)
+def test_empty_post(post: PostService):
+    assert(len(post.get_posts()) == 0)
+
+def test_create_post(post: PostService, test_session: Session, users: UserService):
+    model_user = users.get(999999999)
+    post.create_post(sample_post, model_user)
+    assert(len(post.get_posts()) == 1)
 
 def test_delete_post_valid(post: PostService):
-    post.delete_post(4)
-    assert(len(post.get_posts())==4)
+    post.delete_post(1)
+    assert(len(post.get_posts())==0)
