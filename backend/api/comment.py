fde15cea64c5d85478f7d08ecbbb3c695123da34
Wenjing Huang <rubyhwj@email.unc.edu>
2023-04-14 13:44:32 -0400

    The current version is too outdated without many changes we made, need to merge newly branch

--- backend/api/comment.py
+++ backend/api/comment.py
@@ -3,15 +3,15 @@ from ..services import CommentService
 from ..models import Comment
 
 
-api = APIRouter()
+api = APIRouter(prefix="/api/comment")
 
 #api route retrieces ALL comments
-@api.get("/api/comments")
+@api.get("")
 def get_comments(comment_service: CommentService = Depends()) -> list[Comment]:
     return comment_service.all()
 
 #api route creates a new comment
-@api.post("/api/comment")
+@api.post("")
 def new_comment(comment: Comment, comment_service: CommentService = Depends()) -> Comment:
         try:
             return comment_service.create(comment)
@@ -19,7 +19,7 @@ def new_comment(comment: Comment, comment_service: CommentService = Depends()) -
             raise HTTPException(status_code=422, detail=str(e))
         
 #api route deletes comment
-@api.delete("/api/delete/comment/{id}")
+@api.delete("/{id}")
 def delete_comment(id: int, comment_service = Depends(CommentService)):
     try:
         return comment_service.delete(id)
@@ -27,15 +27,15 @@ def delete_comment(id: int, comment_service = Depends(CommentService)):
         raise HTTPException(status_code=404, detail=str(e))
     
 #api route to update a comment's text
-@api.post("/api/comment/edit")
-def update_comment(comment_id: int, newText: str, comment_service: CommentService = Depends()) -> Comment:
-    try:
-        return comment_service.update(comment_id, newText)
-    except Exception as e:
-        raise HTTPException(status_code=422, detail=str(e))
+# @api.post("/api/comment/edit")
+# def update_comment(comment_id: int, newText: str, comment_service: CommentService = Depends()) -> Comment:
+#     try:
+#         return comment_service.update(comment_id, newText)
+#     except Exception as e:
+#         raise HTTPException(status_code=422, detail=str(e))
     
 #api route to reply to a comment
-@api.post("/api/reply")
+@api.post("/reply")
 def create_reply(comment_id: int, reply: Comment, comment_service: CommentService = Depends()) -> Comment:
     try: 
         return comment_service.reply(comment_id=comment_id,reply=reply)
