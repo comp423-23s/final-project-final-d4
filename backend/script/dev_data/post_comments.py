e179d80722d009256c5be943ca186576e2fccea2
Wenjing Huang <rubyhwj@email.unc.edu>
2023-04-17 12:48:31 -0400

    added some starting data for comments (#86)
    * Create comment.py
    
    * Update __init__.py
    
    * Create comment.py
    
    * The current version is too outdated without many changes we made, need to merge newly branch
    
    * Complete backend comment part including get,create,delete and passed corresponding unit test
    
    * add private comments feature and corresponding unit test
    
    If the private if comment, only the author of the post and the author of the comment and admin can see it. And only  author and admin can delete it.
    
    * Delete comment permission adjustment
    
    Change to check permissions use enforce instead of has_permission for deleteing comments
    
    * add docstring for comment part
    
    * add documentation for comments
    
    * add more on comments documentation
    
    * deactive the create comment part
    
    * add some starting test comments
    
    run reset dataset before you test
    
    * deactivete the delte comment
    
    * Delete oc.exe
    
    ---------
    
    Co-authored-by: Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>

---
+++ backend/script/dev_data/post_comments.py
@@ -0,0 +1,14 @@
+"""Sample posts/comments pairings."""
+
+from . import post,comments
+
+__authors__ = ["Ruby Huang"]
+__copyright__ = "Copyright 2023"
+__license__ = "MIT"
+
+pairs = [
+    (post.post_1, comments.comment_1),
+    (post.post_1, comments.comment_2),
+    (post.post_2, comments.comment_3),
+    (post.post_3, comments.comment_4)
+]
