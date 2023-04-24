422781abffa16a93f9d0b44c20d13a4920cbb1ce
GuningShen <gunings@email.unc.edu>
2023-04-24 17:25:05 -0400

    Adjust the UI of post forum

--- frontend/src/app/post-list/post-list.component.ts
+++ frontend/src/app/post-list/post-list.component.ts
@@ -94,8 +94,8 @@ export class PostListComponent {
     return throwError(err);
   }
   
-  addPrivateComment(){
-    
+  getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
 }
