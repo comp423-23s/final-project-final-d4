3601caf5a192db2ea28129b5b2073b91ebc0bf5c
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-04-28 14:03:39 -0400

    Update comment_entity.py
    delete comments

--- backend/entities/comment_entity.py
+++ backend/entities/comment_entity.py
@@ -3,18 +3,11 @@
 
 from sqlalchemy import Boolean, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-# from sqlalchemy.ext.mutable import MutableList
 from typing import Self
 from datetime import datetime
 from .entity_base import EntityBase
 from .reply_entity import reply_table
 from ..models import Comment
-
-
-# __authors__ = ['Chalisa Phoomsakha']
-# __copyright__ = 'Copyright 2023'
-# __license__ = 'MIT'
-
 
 class CommentEntity(EntityBase):
     __tablename__ = 'comment'
@@ -29,11 +22,6 @@ class CommentEntity(EntityBase):
 
     private : Mapped[bool] = mapped_column(Boolean)
 
-    # not sure ifi this line is necessary, i am combining kris jordan's and my own code
-    # replyTo_id = mapped_column(ForeignKey("comments.id"))
-    # replies: Mapped[list["CommentEntity"]] = relationship(secondary="reply", primaryjoin=id==reply_table.c.comment_id,
-    #                         secondaryjoin=id==reply_table.c.reply_id, back_populates="replies", post_update=True)
-    
     text: Mapped[str] = mapped_column(String(64))
     created: Mapped[datetime] = mapped_column(DateTime)
 
@@ -46,7 +34,6 @@ class CommentEntity(EntityBase):
             user_id=model.commenter,
             post_id=model.post,
             private = model.private
-            # replies=model.replies
         )
 
     def to_model(self) -> Comment:
@@ -57,10 +44,4 @@ class CommentEntity(EntityBase):
             created=self.created,
             post=self.post_id,
             private=self.private
-            # replies=self.replies,
         )
-
-    # not sure if necessary
-    # def update(self, model: Post) -> None:
-    #     self.content = model.content
-    #     self.tags = model.tagss
